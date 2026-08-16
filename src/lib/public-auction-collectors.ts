df9c82081da4fd1ff6a242b84cbed98210fe45b8
microsecret <microsecret@users.noreply.github.com>
2026-08-16 14:16:51 +0500

    expand China model localization

--- src/lib/public-auction-collectors.ts
+++ src/lib/public-auction-collectors.ts
@@ -53,9 +53,14 @@ const CHINESE_MAKES: ReadonlyArray<readonly [string, string]> = [
 ]
 
 const CHINESE_MODEL_TERMS: ReadonlyArray<readonly [RegExp, string]> = [
+  [/200万辆悦享版/g, "юбилейная комплектация Enjoy"],
+  [/40TFSI豪华动感型B&O星夜版/g, "40 TFSI Luxury Dynamic B&O Starry Night"],
+  [/改款领先型M运动套装/g, "рестайлинг Leading, пакет M Sport"],
+  [/动感型运动版/g, "Dynamic Sport"], [/旗舰动感型/g, "Flagship Dynamic"],
+  [/星耀臻藏版/g, "Star Premium"], [/劲势版/g, "Power"], [/思域/g, "Civic"],
   [/(\d{4})款/g, "$1"], [/([A-Z])级/gi, "$1-Class"], [/(\d+)系/g, "$1 Series"],
   [/自动/g, "АКПП"], [/手动/g, "МКПП"], [/前驱/g, "передний привод"], [/后驱/g, "задний привод"],
-  [/四驱|全驱/g, "полный привод"], [/运动版/g, "Sport"], [/时尚版/g, "Style"],
+  [/四驱|全驱/g, "полный привод"], [/运动型|运动版/g, "Sport"], [/时尚版/g, "Style"],
   [/豪华版/g, "Luxury"], [/尊贵版/g, "Premium"], [/旗舰版/g, "Flagship"], [/标准版/g, "Standard"],
   [/舒适版/g, "Comfort"], [/卓越版/g, "Excellence"], [/臻享版/g, "Premium"],
   [/\(国Ⅵ\)|\(国VI\)/gi, "экостандарт China VI"], [/\(国Ⅴ\)|\(国V\)/gi, "экостандарт China V"],
